0e472a08b7aeef0ad3938f1577c2a009f8919d13
sambob1011 <30729772+sambob1011@users.noreply.github.com>
2017-09-25 00:14:43 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2: Lexical Scoping--caching the inverse of a matrix
 
-## Write a short comment describing this function
+## The makeCacheMatrix function creates a special "matrix",
+## which is really a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse of the matrix
+## 4. get the value of the inverse of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  ##initialize inverse property
+  inv <- NULL
+  set <- function(y) {
+   x <<- y
+  inv <<- NULL
+  }
+  ## Method the get the matrix
+  get <- function() x
+  ## Way to set the inverse of the matrix
+  setInverse <- function(inverse) {
+    inv <<- inverse
+  }
+  ## Way to get the inverse of the matrix
+  getInverse <- function() {
+    ## Back the inverse property
+    inv
+  }
+  ## Back a list of the methods
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function calculates the inverse of the special "matrix"
+## the special "matrix" which created with the makeCacheMatrix function.
+## However, it first checks to see if the inverse has already been calculated.
+## If so, it gets the inverse from the cache and skips the computation.
+## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse
+## in the cache via the setinverse function.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) { 
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+if(!is.null(inv)) {
+  message("getting cached data")
+  return(inv)
+}
+data <- x$getInverse()
+inv <- solve(data, ...)
+x$setinverse(inv)
+inv
         ## Return a matrix that is the inverse of 'x'
 }
